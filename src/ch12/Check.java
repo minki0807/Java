1470fd1c8c8e4966321383faccbbd1d517e8c718
minki0807 <myong1306@gmail.com>
2025-03-07 18:41:11 +0900

    test

---
+++ src/ch12/Check.java
@@ -0,0 +1,13 @@
+package src.ch12;
+
+public interface Check {
+        abstract boolean isEven(int n);
+}
+/* class EvenCheck implements Check {
+        public boolean isEven(int n) {
+                if (n%2 == 0)
+                        return true;
+                else
+                        return false;
+        }
+} */
